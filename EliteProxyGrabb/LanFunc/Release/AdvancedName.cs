7ad3c1a2645b2c6c3b9bf39a49194cd3ad018828
Алексей <sysadkrd@gmail.com>
2022-09-20 19:15:31 +0300

    kap

---
+++ EliteProxyGrabb/LanFunc/Release/AdvancedName.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EliteProxyGrabb.LanFunc
+{
+    public class AdvancedName : Finder
+    {
+        public override async Task<Proxy[]> Grab()
+        {
+            List<Proxy> ret = new List<Proxy>();
+            var web = new HtmlWeb();
+            string[] variable = new[]
+            {
+                "elite", "anon", 
+                // "socks4", "socks5"
+
+            };
+            foreach (string s in variable)
+                for (int i = 1; i < 12; i++)
+                {
+                    var doc = web.LoadFromBrowser($"https://advanced.name/ru/freeproxy?type={s}&page={i}");
+                    var tr = doc.GetElementsByTagName("tr").Skip(1).ToList();
+                    foreach (HtmlNode node in tr)
+                    {
+                        try
+                        {
+                            if (node.ChildNodes.Count < 9 && !char.IsDigit(node.ChildNodes[3].InnerText[0])) continue;
+                            var p = new Proxy
+                            {
+
+                                Ip = node.ChildNodes[3].InnerText,
+                                Port = node.ChildNodes[5].InnerText,
+                                Protocol = node.ChildNodes[7].InnerText.Split(' ').First(),
+                            };
+                            ret.Add(p);  
+                        }
+                        catch { }
+                    }
+                    if (ret.Count == 0) break;
+                    OnFinding(ret);
+                }
+            await Task.Delay(2024);
+            return ret.ToArray();
+        }
+
+    }
+}
---
+++ EliteProxyGrabb/LanFunc/Release/FreeProxyList.cs
@@ -0,0 +1,40 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EliteProxyGrabb.LanFunc
+{
+    public class FreeProxyList : Finder
+    {
+        public override async Task<Proxy[]> Grab()
+        {
+            List<Proxy> ret = new List<Proxy>();
+            var web = new HtmlWeb();
+            for (int i = 1; i < 5; i++)
+            {
+                var doc = web.LoadFromBrowser($"https://freeproxylist.ru/proxy-list?page={i}");
+                var tr = doc.GetElementsByTagName("tr").Skip(2).ToList();
+                foreach (HtmlNode node in tr)
+                {
+                    try
+                    {
+                        if (node.ChildNodes.Count < 9 || !char.IsDigit(node.ChildNodes[1].InnerText[0])) 
+                            continue;
+                        var p = new Proxy
+                        {
+                            Ip = node.ChildNodes[1].InnerText,
+                            Port = node.ChildNodes[3].InnerText,
+                            Protocol = node.ChildNodes[7].InnerText,
+                        };
+                        ret.Add(p);
+                    }
+                    catch { }
+                }
+                OnFinding(ret);
+            }
+            await Task.Delay(2024);
+            return ret.ToArray();
+        }
+    }
+}
